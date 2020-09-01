0d6348cf185d9be1142664764a94eb82d745cc80
spiderock <spiderock98@gmail.com>
2020-09-02 00:26:18 +0700

    some changes in js format new extension prettier

--- app.js
+++ app.js
@@ -28,8 +28,10 @@ app.use("/auth", require("./routes/auth-server"));
 
 const admin = require("firebase-admin");
 admin.initializeApp({
-  credential: admin.credential.cert(require("./pairdevices-e7bf9-firebase-adminsdk-tvzye-107bb73eb8.json")),
-  databaseURL: "https://pairdevices-e7bf9.firebaseio.com"
+  credential: admin.credential.cert(
+    require("./pairdevices-e7bf9-firebase-adminsdk-tvzye-107bb73eb8.json")
+  ),
+  databaseURL: "https://pairdevices-e7bf9.firebaseio.com",
 });
 // var db = admin.database();
 // var ref = db.ref("demo");
@@ -44,15 +46,15 @@ const server = require("http").Server(app);
 const io = require("socket.io")(server);
 server.listen(8880);
 
-io.on("connection", socket => {
+io.on("connection", (socket) => {
   // from browser: onLoad() to put browser in to owm room
-  socket.on('regBrowser', () => {
-    socket.join('browser')
-    console.log('[INFO] reg browser successfull');
-  })
+  socket.on("regBrowser", () => {
+    socket.join("browser");
+    console.log("[INFO] reg browser successfull");
+  });
 
   // from esp8266: register new node to server
-  socket.on("regEsp", data => {
+  socket.on("regEsp", (data) => {
     // add some stuff
     data["account"] = "";
     data["connected"] = false;
@@ -62,42 +64,48 @@ io.on("connection", socket => {
   });
 
   // from esp8266: sync virtual button state with physical button state
-  socket.on('controller', data => {
-    admin.database().ref(`${data.uid}/DeviceNodes/${data.physicalName}`).update({
-      state: `${data.state}`
-    })
-    socket.to('browser').emit(`${data.physicalName}`, data.state) // sync custom switch state
-  })
+  socket.on("controller", (data) => {
+    admin
+      .database()
+      .ref(`${data.uid}/DeviceNodes/${data.physicalName}`)
+      .update({
+        state: `${data.state}`,
+      });
+    socket.to("browser").emit(`${data.physicalName}`, data.state); // sync custom switch state
+  });
 
-  socket.on("socketType", data => {
+  socket.on("socketType", (data) => {
     if (data.platform == "browser") {
-      socket.to(data.uid).emit(data.physicalName, `${data.state}`)
-      admin.database().ref(`${data.uid}/DeviceNodes/${data.physicalName}`).update({
-        state: `${data.state}`
-      })
+      socket.to(data.uid).emit(data.physicalName, `${data.state}`);
+      admin
+        .database()
+        .ref(`${data.uid}/DeviceNodes/${data.physicalName}`)
+        .update({
+          state: `${data.state}`,
+        });
     }
   });
 
-  let arrTimeConfig = new Object()
-  socket.on('timeConfig', data => {
-    let index = 0
+  let arrTimeConfig = new Object();
+  socket.on("timeConfig", (data) => {
+    let index = 0;
     Object.values(data.timeObj).forEach((element) => {
       // arrTimeConfig.push(element.substring(0, 2))
       // arrTimeConfig.push(element.substring(3))
       arrTimeConfig[index] = element.substring(0, 2);
-      index += 1
+      index += 1;
       arrTimeConfig[index] = element.substring(3);
-      index += 1
+      index += 1;
     });
     console.log(arrTimeConfig);
 
-    socket.to(data.uid).emit(data.physicalName, arrTimeConfig)
-  })
+    socket.to(data.uid).emit(data.physicalName, arrTimeConfig);
+  });
 
-  socket.on('espcam', img => {
+  socket.on("espcam", (img) => {
     console.log(img);
-    io.emit('espcam', img.data);
-  })
+    // io.emit("espcam", img.data);
+  });
 });
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- public/javascripts/auth-client.js
+++ public/javascripts/auth-client.js
@@ -3,86 +3,103 @@ const socket = io();
 
 // Initialize Firebase
 firebase.initializeApp({
-    apiKey: "AIzaSyAOJyhBMaqhJrTGX3XO7I2WF3kqgElOEM4",
-    authDomain: "pairdevices-e7bf9.firebaseapp.com",
-    databaseURL: "https://pairdevices-e7bf9.firebaseio.com",
-    projectId: "pairdevices-e7bf9",
-    storageBucket: "pairdevices-e7bf9.appspot.com",
-    messagingSenderId: "979300938513",
-    appId: "1:979300938513:web:45ee0e73b4bbfc953192b0"
+  apiKey: "AIzaSyAOJyhBMaqhJrTGX3XO7I2WF3kqgElOEM4",
+  authDomain: "pairdevices-e7bf9.firebaseapp.com",
+  databaseURL: "https://pairdevices-e7bf9.firebaseio.com",
+  projectId: "pairdevices-e7bf9",
+  storageBucket: "pairdevices-e7bf9.appspot.com",
+  messagingSenderId: "979300938513",
+  appId: "1:979300938513:web:45ee0e73b4bbfc953192b0",
 });
 
 firebase.auth().signOut(); //!!!! IMPORTANT
 
-firebase.auth().onAuthStateChanged(user => {
-    if (user) {
-        Swal.showLoading()
-        user.getIdToken().then(idToken => {
-            $.ajax({
-                url: '/auth',
-                method: 'POST',
-                data: { idToken: idToken },
-                success: (data, stt) => {
-                    Swal.fire({
-                        icon: 'success',
-                        title: 'Access Granted'
-                    }).then(() => location.href = '/home')
-                }
-            })
-        }).catch(err => console.log(err))
-    }
-})
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    Swal.showLoading();
+    user
+      .getIdToken()
+      .then((idToken) => {
+        $.ajax({
+          url: "/auth",
+          method: "POST",
+          data: { idToken: idToken },
+          success: (data, stt) => {
+            Swal.fire({
+              icon: "success",
+              title: "Access Granted",
+            }).then(() => (location.href = "/home"));
+          },
+        });
+      })
+      .catch((err) => console.log(err));
+  }
+});
 
 $("#authGoogle").click(() => {
-    firebase.auth().signInWithRedirect(new firebase.auth.GoogleAuthProvider())
-        .then(result => {
-            console.log(result);
-        })
-        .catch(error => console.log(error))
-})
+  firebase
+    .auth()
+    .signInWithRedirect(new firebase.auth.GoogleAuthProvider())
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((error) => console.log(error));
+});
 $("#authFacebook").click(() => {
-    firebase.auth().signInWithRedirect(new firebase.auth.FacebookAuthProvider())
-        .then((result) => {
-            console.log(result)
-        })
-        .catch(error => console.log(error));
-})
-firebase.auth().getRedirectResult().then(result => {
-    let uid = result.user.uid;   // TODO: risk here
+  firebase
+    .auth()
+    .signInWithRedirect(new firebase.auth.FacebookAuthProvider())
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((error) => console.log(error));
+});
+firebase
+  .auth()
+  .getRedirectResult()
+  .then((result) => {
+    let uid = result.user.uid; // TODO: risk here
 
     let idToken = result.credential.idToken;
     let displayName = result.user.displayName;
 
     $.ajax({
-        url: '/auth/thirdParty',
-        method: 'POST',
-        data: { idToken: uid },
-        success: (customToken) => {
-            firebase.auth().signInWithCustomToken(customToken).catch(error => console.log(error))
-        }
-    })
-}).catch(err => console.log(err))
+      url: "/auth/thirdParty",
+      method: "POST",
+      data: { idToken: uid },
+      success: (customToken) => {
+        firebase
+          .auth()
+          .signInWithCustomToken(customToken)
+          .catch((error) => console.log(error));
+      },
+    });
+  })
+  .catch((err) => console.log(err));
 
-$('#loginForm').submit((event) => {
-    event.preventDefault();
-    Swal.showLoading();
+$("#loginForm").submit((event) => {
+  event.preventDefault();
+  Swal.showLoading();
 
-    let id = $('#loginForm').find("input[name='id']").val();
-    let psk = $('#loginForm').find("input[name='psk']").val();
+  let id = $("#loginForm").find("input[name='id']").val();
+  let psk = $("#loginForm").find("input[name='psk']").val();
 
-    // As httpOnly cookies are to be used, do not persist any state client side.
-    // firebase.auth().setPersistence(firebase.auth.Auth.Persistence.NONE);
+  // As httpOnly cookies are to be used, do not persist any state client side.
+  // firebase.auth().setPersistence(firebase.auth.Auth.Persistence.NONE);
 
-    // sign in
-    firebase.auth().signInWithEmailAndPassword(id, psk).catch((error) => {
-        Swal.fire({
-            title: error,
-            icon: 'error',
-        });
+  // sign in
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(id, psk)
+    .catch((error) => {
+      Swal.fire({
+        title: error,
+        icon: "error",
+      });
 
-        // firebase.auth().createUserWithEmailAndPassword(id, psk)
-        //     .then(() => {
-        //         window.alert('Successfully Sign Up')
-        //     }).catch(err => console.log(err))
-    })
-})
+      // firebase.auth().createUserWithEmailAndPassword(id, psk)
+      //     .then(() => {
+      //         window.alert('Successfully Sign Up')
+      //     }).catch(err => console.log(err))
+    });
+});
