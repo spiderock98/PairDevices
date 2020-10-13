ae9b0ea6270c05a3da67505d53b84f44fca0c08e
spiderock <spiderock98@gmail.com>
2020-10-13 16:34:28 +0700

    update espCamTest to main-branch

--- ESP32-CamAI/Arduino.ESP32.Camera/src/main.cpp
+++ ESP32-CamAI/Arduino.ESP32.Camera/src/main.cpp
@@ -1,28 +1,61 @@
+#define DEBUG true
+#define TEST true
+
 #include <Arduino.h>
-#include "esp_camera.h"
 #include <WiFi.h>
-#include <WiFiClientSecure.h>
 #include <WebSocketsClient.h>
-
+#include <ArduinoJson.h>
+#include "esp_camera.h"
 #define CAMERA_MODEL_AI_THINKER
-
+#if TEST == true
 #include "camera_pins.h"
+#else
+#include "include/camera_pins.h"
+#endif
 
 WebSocketsClient webSocket;
 
-#define USE_SERIAL Serial
+bool flagEnCam = false;
 
-const char *ssid = "VIETTEL";
-const char *password = "Sherlock21vtag";
+#if TEST == true
+#define STASSID "VIETTEL"
+#define STAPSK "Sherlock21vtag"
+// bApb0Ypwg5YszGanWOBKre39zlg1
+#define UID "bApb0Ypwg5YszGanWOBKre39zlg1"
+#define NODENAME "vuon xoai"
+#define HOST "192.168.1.5" //HOME local ip
+#define PORT 81
+#define LED_BUILTIN 33
+#define FLASH_BUILTIN 4
+#else
+#define STASSID "taikhoan"
+#define STAPSK "matkhau"
+// bApb0Ypwg5YszGanWOBKre39zlg1
+#define UID "dinhdanh"
+#define NODENAME "physicalId"
+#define HOST "192.168.1.4" //HOME local ip
+#define PORT 81
+#define LED_BUILTIN 33
+#define FLASH_BUILTIN 4
+#endif
 
-void startCameraServer();
+const char *ssid = STASSID;
+const char *password = STAPSK;
+const char *uid = UID;
+const char *nodename = NODENAME;
+
+String jsonOut;
+void webSocketEventHandle(WStype_t type, uint8_t *payload, size_t length);
 
 void setup()
 {
+#if DEBUG == true
   Serial.begin(115200);
   Serial.setDebugOutput(true);
-  Serial.println();
+#endif
+  pinMode(LED_BUILTIN, OUTPUT);
 
+  //!================/ AI-Thinker Camera Config /================!//
   camera_config_t config;
   config.ledc_channel = LEDC_CHANNEL_0;
   config.ledc_timer = LEDC_TIMER_0;
@@ -44,7 +77,7 @@ void setup()
   config.pin_reset = RESET_GPIO_NUM;
   config.xclk_freq_hz = 20000000;
   config.pixel_format = PIXFORMAT_JPEG;
-  //init with high specs to pre-allocate larger buffers
+  //? init with high specs to pre-allocate larger buffers
   if (psramFound())
   {
     // change format and quality here
@@ -58,54 +91,140 @@ void setup()
     config.jpeg_quality = 12;
     config.fb_count = 1;
   }
-
-  // camera init
+  //? camera init
   esp_err_t err = esp_camera_init(&config);
   if (err != ESP_OK)
   {
+#if DEBUG == true
     Serial.printf("Camera init failed with error 0x%x", err);
+#endif
     return;
   }
 
+  //!================/ Internet Config /================!//
   WiFi.begin(ssid, password);
-
   while (WiFi.status() != WL_CONNECTED)
   {
+    digitalWrite(LED_BUILTIN, 1);
     delay(100);
-    Serial.print(".");
+    digitalWrite(LED_BUILTIN, 0);
+    delay(100);
   }
-  Serial.println("");
-  Serial.println("WiFi connected");
-
-  // startCameraServer();
-
+#if DEBUG == true
+  Serial.println("\nWiFi connected");
   Serial.print("Camera Ready! Use 'http://");
   Serial.print(WiFi.localIP());
   Serial.println("' to connect");
-
-  webSocket.begin("192.168.1.2", 81, "/"); // server address, port and URL
-
-  // webSocket.setReconnectInterval(5000); // try ever 5000 again if connection has failed
+#endif
+  //!================/ Init JSON Garden Infomation /================!//
+  DynamicJsonDocument doc(1024);
+  JsonArray array = doc.to<JsonArray>();
+  // array.add("regEsp");
+  JsonObject param1 = array.createNestedObject(); // add payload (parameters) for the event
+  //TODO: nếu nhấn btn thì pair | re-pair không thì ko regEsp
+  // param1["EVENT"] = "regESP";
+  param1["EVENT"] = "espEnCamera";
+  param1["MAC"] = WiFi.macAddress();
+  param1["IP"] = WiFi.localIP().toString();
+  param1["SSID"] = WiFi.SSID();
+  param1["PSK"] = WiFi.psk();
+  param1["UID"] = uid;
+  serializeJson(doc, jsonOut);
+  //!================/ WebSocket Config /================!//
+  webSocket.begin(HOST, PORT, "/"); // server address, port and URL
+  webSocket.onEvent(webSocketEventHandle);
 }
 
 void loop()
 {
-  camera_fb_t *fb = esp_camera_fb_get();
-  if (!fb)
-  {
-    Serial.println("[INFO] Capture failed");
-    esp_camera_fb_return(fb);
-    return;
-  }
-  if (fb->format != PIXFORMAT_JPEG)
-  {
-    Serial.println("[INFO] None-JPEG data");
-    return;
-  }
-  // Serial.println((const char *)fb->buf);
+  //     if (flagEnCam)
+  //     {
+  //         camera_fb_t *fb = esp_camera_fb_get();
+  //         if (!fb)
+  //         {
+  // #if DEBUG == true
+  //             Serial.println("[INFO] Capture failed");
+  // #endif
+  //             esp_camera_fb_return(fb);
+  //             return;
+  //         }
+  //         if (fb->format != PIXFORMAT_JPEG)
+  //         {
+  // #if DEBUG == true
+  //             Serial.println("[INFO] None-JPEG data");
+  // #endif
+  //             return;
+  //         }
+  //         webSocket.sendBIN(fb->buf, fb->len); // send message to server when Connected
 
-  webSocket.sendBIN(fb->buf, fb->len); // send message to server when Connected
-  esp_camera_fb_return(fb);
+  //         esp_camera_fb_return(fb);
+  //     }
 
   webSocket.loop();
+}
+
+//!================/ Func Define /================!//
+void webSocketEventHandle(WStype_t type, uint8_t *payload, size_t length)
+{
+  switch (type)
+  {
+  case WStype_DISCONNECTED:
+#if DEBUG == true
+    Serial.printf("[WSc] Disconnected!\n");
+#endif
+    break;
+
+  case WStype_CONNECTED:
+#if DEBUG == true
+    Serial.printf("[WSc] Connected to url: %s\n", payload);
+#endif
+    //? ====/ send message to server when Connected /==== ?//
+    webSocket.sendTXT(jsonOut);
+    break;
+
+    //!=====/ on recieve data /=====!//
+  case WStype_TEXT:
+#if DEBUG == true
+    Serial.printf("[WSc] get text: %s\n", payload);
+#endif
+    StaticJsonDocument<1024> recvDoc;
+    DeserializationError error = deserializeJson(recvDoc, payload, length);
+    //? Test if parsing succeeds.
+    if (error)
+    {
+#if DEBUG == true
+      Serial.print(F("[ERROR] deserializeJson() failed"));
+#endif
+      return;
+    }
+    else
+    {
+      String eventName = recvDoc["EVENT"];
+      if (eventName == "browserEnCam")
+      {
+        flagEnCam = true;
+#if DEBUG == true
+        Serial.println("[INFO] START Streaming ...");
+#endif
+      }
+      else if (eventName == "browserDisCam")
+      {
+        flagEnCam = false;
+#if DEBUG == true
+        Serial.println("[INFO] STOP Streaming ...");
+#endif
+      }
+    }
+
+    //? Fetch values.
+    // char json[] = "{\"sensor\":\"gps\",\"time\":1351824120,\"data\":[48.756080,2.302038]}";
+    // Most of the time, you can rely on the implicit casts.
+    // In other case, you can do doc["time"].as<long>();
+    // const char *sensor = recvDoc["sensor"];
+    // long time = recvDoc["time"];
+    // double latitude = recvDoc["data"][0];
+    // double longitude = recvDoc["data"][1];
+
+    break;
+  }
 }
